331f403824b362276bd7e9cdc65c5ea79306d391
dimitrisx26 <dxynos@outlook.com>
2024-09-30 00:58:39 +0300

    add user profile view component and update layout styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { UserProfileViewComponent } from "./user-profile/user-profile-view/user-profile-view.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MenuComponent, RouterOutlet],
+  imports: [CommonModule, MenuComponent, RouterOutlet, UserProfileViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/user-profile/user-profile-view/user-profile-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-profile-view.component.html',
+  styleUrl: './user-profile-view.component.css'
+})
+export class UserProfileViewComponent {
+
+}
