9db28caaaaf7bd9b0e25d7dacf05f419ff4cda56
dimitrisx26 <dxynos@outlook.com>
2024-09-30 01:21:27 +0300

    structure the app better, create the necessary components for user mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './components/menu/menu.component';
-import { UserProfileViewComponent } from './components/profile/user-profile-view/profile-view.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,6 @@ import { UserProfileViewComponent } from './components/profile/user-profile-view
     CommonModule,
     MenuComponent,
     RouterOutlet,
-    UserProfileViewComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
