66c8902922caeb30c104d0bb76a8bdd91a315658
dimitrisx26 <dxynos@outlook.com>
2024-09-30 21:55:52 +0300

    update view component to include header input and enhance styles

--- src/app/components/shared/view/view.component.ts
+++ src/app/components/shared/view/view.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view.component.html',
   styleUrl: './view.component.css'
 })
 export class ViewComponent {
-
+  /** Contains the view header info */
+  @Input() header!: string;
 }
