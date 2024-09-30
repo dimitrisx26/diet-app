39c4b26a8dc3e93aa3a26587536238f74bb5f22d
dimitrisx26 <dxynos@outlook.com>
2024-09-30 23:45:00 +0300

    add panels to profile view

--- src/app/components/user/profile/user-profile-view/profile-view.component.ts
+++ src/app/components/user/profile/user-profile-view/profile-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ViewComponent } from "../../../shared/view/view.component";
+import { PanelModule } from "primeng/panel";
 
 @Component({
   selector: 'app-profile-view',
   standalone: true,
-  imports: [ViewComponent],
+  imports: [ViewComponent, PanelModule],
   templateUrl: './profile-view.component.html',
   styleUrl: './profile-view.component.css'
 })
