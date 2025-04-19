dec6a58c691a837842fc17f2a65f5c33b088d849
dimitrisx26 <dxynos@outlook.com>
2025-04-19 22:15:22 +0300

    update routing to use lazy loading for components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,26 @@
 import { Routes } from '@angular/router';
-import { ProfileViewComponent } from './components/user/profile/user-profile-view/profile-view.component';
-import { DietPlanViewComponent } from './components/shared/diet-plan/diet-plan-view/diet-plan-view.component';
-import { ChatViewComponent } from './components/shared/chat/chat-view/chat-view.component';
-import { AppointmentViewComponent } from './components/user/appointment/appointment-view/appointment-view.component';
-
 
 export const routes: Routes = [
-  { path: 'profile', component: ProfileViewComponent },
-  { path: 'diet-plan', component: DietPlanViewComponent},
-  { path: 'chat', component: ChatViewComponent},
-  { path: 'appointment', component: AppointmentViewComponent},
-  // { path: 'login', component: LoginComponent },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./components/user/profile/user-profile-view/profile-view.component').then(m => m.ProfileViewComponent)
+  },
+  {
+    path: 'diet-plan',
+    loadComponent: () =>
+      import('./components/shared/diet-plan/diet-plan-view/diet-plan-view.component').then(m => m.DietPlanViewComponent)
+  },
+  {
+    path: 'chat',
+    loadComponent: () =>
+      import('./components/shared/chat/chat-view/chat-view.component').then(m => m.ChatViewComponent)
+  },
+  {
+    path: 'appointment',
+    loadComponent: () =>
+      import('./components/user/appointment/appointment-view/appointment-view.component').then(m => m.AppointmentViewComponent)
+  },
   { path: '', redirectTo: '/profile', pathMatch: 'full' },
   { path: '**', redirectTo: '/profile' },
 ];
