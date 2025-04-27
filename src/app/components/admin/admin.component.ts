bf1e06fe77854fcd4faf88c6a93181579ed95759
dimitrisx26 <dxynos@outlook.com>
2025-04-27 15:00:07 +0300

    add user growth chart to admin panel; integrate Chart.js for visualizing user creation data

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -7,11 +7,13 @@ import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { map } from 'rxjs';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-admin',
   imports: [
     ButtonModule,
+    ChartModule,
     CommonModule,
     ViewComponent,
     PanelModule,
@@ -33,11 +35,58 @@ export class AdminComponent {
   /** The list of users created in the last 7 days */
   recentUsers$;
 
+  /** Chart data for users created */
+  userGrowthChartData = {
+    labels: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+    datasets: [
+      {
+        label: 'Users',
+        data: [''],
+        fill: false,
+        borderColor: '#22c55e',
+        tension: 0.4,
+      },
+    ],
+  };
+
+  /** Options for the user growth chart */
+  userGrowthChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
   constructor(
     private admin: AdminService,
     private auth: AuthService,
   ) {
     this.recentUsers$ = this.recentlyCreatedUsers();
+  }
+
+  ngOnInit() {
+    this.usersPerMonth();
   }
 
   /**
@@ -63,5 +112,31 @@ export class AdminComponent {
         }).length;
       }),
     );
+  }
+
+  usersPerMonth() {
+    const usersFiltered = this.users$.pipe(
+      map((users) => {
+        const now = new Date();
+        const monthCounts = Array(12).fill(0);
+        users.forEach((user) => {
+          const createdAt = new Date(user.$createdAt);
+          if (createdAt.getFullYear() === now.getFullYear()) {
+            monthCounts[createdAt.getMonth()]++;
+          }
+        });
+        return monthCounts;
+      }),
+    );
+
+    return usersFiltered.subscribe({
+      next: (users) => {
+        this.userGrowthChartData.datasets[0].data = users;
+        this.userGrowthChartData = { ...this.userGrowthChartData };
+      },
+      error: (err) => {
+        console.error('Error fetching user growth data:', err);
+      }
+    });
   }
 }
